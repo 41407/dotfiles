14327ad19dcfc19d76407909a22789a3010c84b0
41407 <jimi.welling@cs.helsinki.fi>
2017-08-10 22:50:39 +0300

    Tweak aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -63,7 +63,7 @@ fi
 alias :q='exit'
 
 # vimrc editing
-alias ve='vim ~/.vimrc'
+# alias ve='vim ~/.vimrc'
 
 # zsh profile editing
 alias ze='vim ~/.zshrc'
@@ -75,12 +75,11 @@ alias gst='git stash'
 alias gsp='git stash pop'
 alias gsa='git stash apply'
 alias gsh='git show'
+alias gshs='git show --stat'
 alias gshw='git show'
 alias gshow='git show'
-alias gi='vim .gitignore'
-alias gcm='git ci -m'
-alias gcim='git ci -m'
-alias gci='git ci'
+#alias gi='vim .gitignore'
+alias gcm='git commit -m'
 alias gco='git co'
 alias gcp='git cp'
 alias ga='git add -A'
@@ -100,7 +99,7 @@ alias gbi='git rebase --interactive'
 alias gl='git l'
 alias glg='git l'
 alias glog='git l'
-alias co='git co'
+#alias co='git co'
 alias gf='git fetch'
 alias gfp='git fetch --prune'
 alias gfa='git fetch --all'
@@ -111,21 +110,22 @@ alias gb='git b'
 # Staged and cached are the same thing
 alias gdc='git diff --cached -w'
 alias gds='git diff --staged -w'
-alias gpub='grb publish'
-alias gtr='grb track'
+#alias gpub='grb publish'
+#alias gtr='grb track'
 alias gpl='git pull'
 alias gplr='git pull --rebase'
 alias gps='git push'
 alias gpsh='git push -u origin `git rev-parse --abbrev-ref HEAD`'
 alias gnb='git nb' # new branch aka checkout -b
+alias gchb='git nb' # same as above
 alias grs='git reset'
 alias grsh='git reset --hard'
 alias gcln='git clean'
 alias gclndf='git clean -df'
 alias gclndfx='git clean -dfx'
-alias gsm='git submodule'
-alias gsmi='git submodule init'
-alias gsmu='git submodule update'
+#alias gsm='git submodule'
+#alias gsmi='git submodule init'
+#alias gsmu='git submodule update'
 alias gt='git t'
 alias gbg='git bisect good'
 alias gbb='git bisect bad'
@@ -143,22 +143,22 @@ alias cl='clear'
 alias gz='tar -zcvf'
 
 # Ruby
-alias c='rails c' # Rails 3
-alias co='script/console' # Rails 2
-alias cod='script/console --debugger'
+#alias c='rails c' # Rails 3
+#alias co='script/console' # Rails 2
+#alias cod='script/console --debugger'
 
 #If you want your thin to listen on a port for local VM development
 #export VM_IP=10.0.0.1 <-- your vm ip
-alias ts='thin start -a ${VM_IP:-127.0.0.1}'
-alias ms='mongrel_rails start'
-alias tfdl='tail -f log/development.log'
-alias tftl='tail -f log/test.log'
+#alias ts='thin start -a ${VM_IP:-127.0.0.1}'
+#alias ms='mongrel_rails start'
+#alias tfdl='tail -f log/development.log'
+#alias tftl='tail -f log/test.log'
 
-alias ka9='killall -9'
-alias k9='kill -9'
+#alias ka9='killall -9'
+#alias k9='kill -9'
 
 # Gem install
-alias sgi='sudo gem install --no-ri --no-rdoc'
+#alias sgi='sudo gem install --no-ri --no-rdoc'
 
 # TODOS
 # This uses NValt (NotationalVelocity alt fork) - http://brettterpstra.com/project/nvalt/
@@ -166,48 +166,48 @@ alias sgi='sudo gem install --no-ri --no-rdoc'
 alias todo='open nvalt://find/todo'
 
 # Forward port 80 to 3000
-alias portforward='sudo ipfw add 1000 forward 127.0.0.1,3000 ip from any to any 80 in'
+#alias portforward='sudo ipfw add 1000 forward 127.0.0.1,3000 ip from any to any 80 in'
 
-alias rdm='rake db:migrate'
-alias rdmr='rake db:migrate:redo'
+#alias rdm='rake db:migrate'
+#alias rdmr='rake db:migrate:redo'
 
 # Zeus
-alias zs='zeus server'
-alias zc='zeus console'
-alias zr='zeus rspec'
-alias zrc='zeus rails c'
-alias zrs='zeus rails s'
-alias zrdbm='zeus rake db:migrate'
-alias zrdbtp='zeus rake db:test:prepare'
-alias zzz='rm .zeus.sock; pkill zeus; zeus start'
+#alias zs='zeus server'
+#alias zc='zeus console'
+#alias zr='zeus rspec'
+#alias zrc='zeus rails c'
+#alias zrs='zeus rails s'
+#alias zrdbm='zeus rake db:migrate'
+#alias zrdbtp='zeus rake db:test:prepare'
+#alias zzz='rm .zeus.sock; pkill zeus; zeus start'
 
 # Rspec
-alias rs='rspec spec'
-alias sr='spring rspec'
-alias src='spring rails c'
-alias srgm='spring rails g migration'
-alias srdm='spring rake db:migrate'
-alias srdt='spring rake db:migrate'
-alias srdmt='spring rake db:migrate db:test:prepare'
+#alias rs='rspec spec'
+#alias sr='spring rspec'
+#alias src='spring rails c'
+#alias srgm='spring rails g migration'
+#alias srdm='spring rake db:migrate'
+#alias srdt='spring rake db:migrate'
+#alias srdmt='spring rake db:migrate db:test:prepare'
 
 
 # Sprintly - https://github.com/nextbigsoundinc/Sprintly-GitHub
-alias sp='sprintly'
+#alias sp='sprintly'
 # spb = sprintly branch - create a branch automatically based on the bug you're working on
-alias spb="git checkout -b \`sp | tail -2 | grep '#' | sed 's/^ //' | sed 's/[^A-Za-z0-9 ]//g' | sed 's/ /-/g' | cut -d"-" -f1,2,3,4,5\`"
+#alias spb="git checkout -b \`sp | tail -2 | grep '#' | sed 's/^ //' | sed 's/[^A-Za-z0-9 ]//g' | sed 's/ /-/g' | cut -d"-" -f1,2,3,4,5\`"
 
-alias hpr='hub pull-request'
-alias grb='git recent-branches'
+#alias hpr='hub pull-request'
+#alias grb='git recent-branches'
 
 # Finder
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
 
-alias dbtp='spring rake db:test:prepare'
-alias dbm='spring rake db:migrate'
-alias dbmr='spring rake db:migrate:redo'
-alias dbmd='spring rake db:migrate:down'
-alias dbmu='spring rake db:migrate:up'
+#alias dbtp='spring rake db:test:prepare'
+#alias dbm='spring rake db:migrate'
+#alias dbmr='spring rake db:migrate:redo'
+#alias dbmd='spring rake db:migrate:down'
+#alias dbmu='spring rake db:migrate:up'
 
 # Homebrew
 alias brewu='brew update  && brew upgrade && brew cleanup && brew prune && brew doctor'
