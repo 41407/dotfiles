618db3981bb9310364095bb799e5b53fcaf78ad9
Jimi Welling <jimi.welling@kuuasema.com>
2017-12-27 14:23:17 +0200

    Add sublime cli installation script

---
+++ sublime/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Add `subl` to /usr/local/bin as `sublime`.
+# Source: http://olivierlacan.com/posts/launch-sublime-text-3-from-the-command-line/
+
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
