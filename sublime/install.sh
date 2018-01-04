9c5802e1929ae8d07fbc7f278480efe0043fc6db
Jimi W <jimi.welling@kuuasema.com>
2018-01-04 19:58:30 +0200

    Add echoes to subl install

--- sublime/install.sh
+++ sublime/install.sh
@@ -2,4 +2,6 @@
 # Add `subl` to /usr/local/bin as `sublime`.
 # Source: http://olivierlacan.com/posts/launch-sublime-text-3-from-the-command-line/
 
+echo "Linking subl"
 ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
+echo "Done!"
